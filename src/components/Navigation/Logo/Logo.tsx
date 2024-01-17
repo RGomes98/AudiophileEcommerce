26a1673f0ad1486614b95a393f36dfb9740c3101
Renan Gomes <renango2015@gmail.com>
2024-01-16 21:14:00 -0300

    refactor: increase logo image quality

--- src/components/Navigation/Logo/Logo.tsx
+++ src/components/Navigation/Logo/Logo.tsx
@@ -5,7 +5,14 @@ import Link from 'next/link';
 export const Logo = () => {
   return (
     <Link className={styles.link} href='/'>
-      <Image className={styles.logo} src='/assets/logo.svg' alt='logo' width={145} height={25} />
+      <Image
+        className={styles.logo}
+        src='/assets/logo.svg'
+        alt='logo'
+        width={145}
+        height={25}
+        quality={100}
+      />
     </Link>
   );
 };
