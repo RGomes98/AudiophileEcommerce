ea6a9e21f58a25d0ddc332f8940c355f4b5c0105
Renan Gomes <renango2015@gmail.com>
2024-01-16 21:20:06 -0300

    build: fix deploy warnings

--- src/components/InteractionHub/InteractionHub.tsx
+++ src/components/InteractionHub/InteractionHub.tsx
@@ -1,11 +1,12 @@
+// import { FeaturedProducts } from './FeaturedProducts/FeaturedProducts';
 import { CategoryPanel } from './CategoryPanel/CategoryPanel';
-import { FeaturedProducts } from './FeaturedProducts/FeaturedProducts';
 import styles from './InteractionHub.module.scss';
 
 export const InteractionHub = () => {
   return (
     <section className={styles.container}>
       <CategoryPanel />
+      {/* <FeaturedProducts /> */}
     </section>
   );
 };
