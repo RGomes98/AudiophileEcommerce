774a88316fdb90a09a26a46c3555916a1b1666e8
Renan Gomes <renango2015@gmail.com>
2024-01-16 21:10:23 -0300

    feat: add featured section

---
+++ src/components/Featured/Featured.tsx
@@ -0,0 +1,20 @@
+import styles from './Featured.module.scss';
+import Link from 'next/link';
+
+export const Featured = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.wrapper}>
+        <span className={styles.text}>New product</span>
+        <h1 className={styles.name}>XX99 Mark II Headphones</h1>
+        <p className={styles.description}>
+          Experience natural, lifelike audio and exceptional build quality made for the passionate music
+          enthusiast.
+        </p>
+        <Link className={styles.link} href='#'>
+          See Product
+        </Link>
+      </div>
+    </section>
+  );
+};
