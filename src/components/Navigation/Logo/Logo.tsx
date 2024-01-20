b3cc34aa9e3278f97a40757966c8df939590872c
Renan Gomes <renango2015@gmail.com>
2024-01-20 20:59:52 -0300

    feat: add close dropdown on logo click

--- src/components/Navigation/Logo/Logo.tsx
+++ src/components/Navigation/Logo/Logo.tsx
@@ -1,10 +1,15 @@
+'use client';
+
+import { useAudiophileContext } from '@/hooks/useAudiophileContext';
 import styles from './Logo.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export const Logo = () => {
+  const { setIsDropdownActive } = useAudiophileContext();
+
   return (
-    <Link className={styles.link} href='/'>
+    <Link className={styles.link} onClick={() => setIsDropdownActive(false)} href='/'>
       <Image
         className={styles.logo}
         src='/assets/logo.svg'
