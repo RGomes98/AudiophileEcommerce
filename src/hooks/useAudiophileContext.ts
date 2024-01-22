8363f362fa9be516d8b365d799a840097ec65c67
Renan Gomes <renango2015@gmail.com>
2024-01-21 22:34:10 -0300

    refactor: add error handling for useAudiophileContext

--- src/hooks/useAudiophileContext.ts
+++ src/hooks/useAudiophileContext.ts
@@ -1,4 +1,8 @@
 import { Context } from '@/context/Context';
 import { useContext } from 'react';
 
-export const useAudiophileContext = () => useContext(Context);
+export const useAudiophileContext = () => {
+  const audiophileContext = useContext(Context);
+  if (!audiophileContext) throw new Error('Use Audiophile context within its provider.');
+  return audiophileContext;
+};
