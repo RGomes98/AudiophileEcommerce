69cba5ecc1d541791636e8aebcba5b2034b455b2
Renan Gomes <renango2015@gmail.com>
2024-01-21 22:32:08 -0300

    refactor: clearer constant names for schemas

--- src/lib/zod/schemas/product.schema.ts
+++ src/lib/zod/schemas/product.schema.ts
@@ -2,54 +2,54 @@ import { z } from 'zod';
 
 export const categories = ['headphones', 'speakers', 'earphones'] as const;
 
-export const image = z.object({
+export const imageSchema = z.object({
   mobile: z.string(),
   tablet: z.string(),
   desktop: z.string(),
   cart: z.string(),
 });
 
-export const categoryImage = image.omit({
+export const categoryImageSchema = imageSchema.omit({
   cart: true,
 });
 
-export const include = z.object({
+export const includeSchema = z.object({
   quantity: z.number(),
   item: z.string(),
 });
 
-export const gallery = z.object({
-  first: categoryImage,
-  second: categoryImage,
-  third: categoryImage,
+export const gallerySchema = z.object({
+  first: categoryImageSchema,
+  second: categoryImageSchema,
+  third: categoryImageSchema,
 });
 
-export const other = z.object({
+export const otherSchema = z.object({
   slug: z.string(),
   name: z.string(),
-  image: categoryImage,
+  image: categoryImageSchema,
   category: z.enum(categories),
 });
 
-export const product = z.object({
+export const productSchema = z.object({
   id: z.number(),
   slug: z.string(),
   name: z.string(),
-  image: image,
+  image: imageSchema,
   category: z.enum(categories),
-  categoryImage: categoryImage,
+  categoryImage: categoryImageSchema,
   new: z.boolean(),
   price: z.number(),
   description: z.string(),
   features: z.string(),
-  includes: z.array(include),
-  gallery: gallery,
-  others: z.array(other),
+  includes: z.array(includeSchema),
+  gallery: gallerySchema,
+  others: z.array(otherSchema),
 });
 
-export type CategoryImage = z.infer<typeof categoryImage>;
-export type ProductImage = z.infer<typeof image>;
-export type Include = z.infer<typeof include>;
-export type Gallery = z.infer<typeof gallery>;
-export type Product = z.infer<typeof product>;
-export type Other = z.infer<typeof other>;
+export type CategoryImage = z.infer<typeof categoryImageSchema>;
+export type ProductImage = z.infer<typeof imageSchema>;
+export type Include = z.infer<typeof includeSchema>;
+export type Gallery = z.infer<typeof gallerySchema>;
+export type Product = z.infer<typeof productSchema>;
+export type Other = z.infer<typeof otherSchema>;
